7316f4c8f2a82aaee712dde77da3aba493eba6e2
ChenBlue <bob840806@gmail.com>
2018-02-05 23:57:27 +0800

    modify size of image & add comment

--- main.m
+++ main.m
@@ -8,5 +8,8 @@ phir_intf = pi/2; % reception direction of interference signal
 phit = pi/6; % angle of LOS onto tx antenna
 phit_intf = pi/3; % radiation direction of interference signal
 
-%[U_SIMO, desired_gain_SIMO, SINR_SIMO, SINR_MRC_SIMO] = SIMO(Nr, deltar, phir, phir_intf);
+%%% SIMO model
+[U_SIMO, desired_gain_SIMO, SINR_SIMO, SINR_MRC_SIMO] = SIMO(Nr, deltar, phir, phir_intf);
+
+%%% MISO model 
 [U_MISO, desired_gain_MISO, SINR_MISO, SINR_MRC_MISO] = MISO(Nt, deltat, phit, phit_intf);
